d34a5429d28a89e43f9985a1f5990b96e13ae51c
jhonhewlly <60392404+jhonhewlly@users.noreply.github.com>
2022-06-30 23:26:58 -0300

    Update NivelLigivel.c

--- NivelLigivel.c
+++ NivelLigivel.c
@@ -1,14 +1,14 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
-void leg(char *texto, int *vet){
-printf("Hello world");
+void leg(char *texto, int *vet,int len){
+printf("%d\n",vet[2]);
 }
 int main() {
     char string[200];
     int vetor[]={0,0,0};
     // Write C code here
-    leg(string,vetor);
+    leg(string,vetor,0);
     
     return 0;
 }
