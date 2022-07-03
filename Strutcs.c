5cbbdd8b65e9069bb0d06583daac6c435cbe6727
jhonhewlly <60392404+jhonhewlly@users.noreply.github.com>
2022-07-02 22:50:49 -0300

    Update Strutcs.c

--- Strutcs.c
+++ Strutcs.c
@@ -6,6 +6,16 @@ typedef struct{
     char rua[100];
     int numero;
 } endereco;
+void addres(endereco *ent){
+    
+    printf("Insira a Cidade:\n");
+    scanf("%s",ent->cidade);
+    printf("Insira uma Rua:\n");
+    scanf("%s",ent->rua);
+    printf("insira um numero:\n");
+    scanf("%d",ent->numero);
+    
+}
  struct pessoa {
     char nome[20];
     int idade;
@@ -14,6 +24,8 @@ typedef struct{
 } ;
 struct pessoa cadastro(){
     struct pessoa x;
+    
+    char f[3];
     printf("Ensira o nome:\n");
     scanf("%s",x.nome);
     printf("Ensira a Idade:\n");
@@ -22,10 +34,13 @@ struct pessoa cadastro(){
     scanf("%.2f",&x.altura);
     return x;
 }
+
 int main() {
    struct pessoa r;
+   endereco xc;
     strcpy(r.nome,"Augusto");
     cadastro();
+    addres(&xc);
     // Write C code here
     printf("%s",r.nome);
     
